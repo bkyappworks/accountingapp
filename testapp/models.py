f8aab70cf71609985e8a83ce1411d33090b59bd2
Becky Liu <beckyliu@beckys-air.wifi.local.cmu.edu>
2023-03-24 16:26:17 -0400

    test connection with postgres and insert test data

--- testapp/models.py
+++ testapp/models.py
@@ -1,24 +1,27 @@
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
+# import json
 
 # Create your models here.
+class Users(models.Model):
+    pass
 
 class Account(models.Model):
     id = models.AutoField(primary_key=True)
     account_number = models.CharField(max_length=16, unique=True)
     current_balance = models.DecimalField(max_digits=10, decimal_places=2)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='accounts')
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='accounts')
     
     def __str__(self):
         return self.account_number
 
-class UserProfile(AbstractUser):
-    REQUIRED_FIELDS = ['email']
-    # one userprofile only link with one user
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    # refered in user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
-    account_set = models.ManyToManyField(Account)
+# class UserProfile(AbstractUser):
+#     REQUIRED_FIELDS = ['email']
+#     # one userprofile only link with one user
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+#     # refered in user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
+#     account_set = models.ManyToManyField(Account)
 
 class Transaction(models.Model):
     id = models.AutoField(primary_key=True)
@@ -32,4 +35,11 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
+# class UserProfile(models.Model):
+#     # one userprofile only link with one user
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     # refered in user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
+#     accounts = models.ManyToManyField(Account)
 
+#     def __str__(self):
+#         return self.user.username
