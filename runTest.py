40dc47413dc570155b659078f94f3af1e83fcc73
Becky Liu <beckyliu@beckys-air.wifi.local.cmu.edu>
2023-03-24 17:32:12 -0400

    recreate new db to fix missing tables and migrations issues

--- runTest.py
+++ runTest.py
@@ -9,16 +9,16 @@ django.setup()
 from django.contrib.auth.models import User
 from testapp.models import Account, Transaction
 
-# Create a test user
-# user = User.objects.create_user(username='testuser', password='password')
-
-
 # The following can only be run once
+
+# Create a test user
+user1 = User.objects.create_user(username='testuser1', password='password1')
 
 # Create test accounts
 
-# Account.objects.create(account_number='1234567890', current_balance=1000.00)
-# Account.objects.create(account_number='0987654321', current_balance=500.00)
+Account.objects.create(account_number='1234567890', current_balance=1000.00,user = user1)
+Account.objects.create(account_number='0987654321', current_balance=500.00,user = user1)
 
 # Create test transactions
 Transaction.objects.create(date='2021-01-01', transaction_type='CREDIT', note='test', amount=100.00, account=Account.objects.get(account_number='1234567890'))
+Transaction.objects.create(date='2021-01-01', transaction_type='CREDIT', note='test', amount=200.00, account=Account.objects.get(account_number='0987654321'))
