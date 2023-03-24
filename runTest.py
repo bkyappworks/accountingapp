8b7dfb8f83938c465ed0e4f8119e2ab2da1a4e1e
Becky Liu <beckyliu@beckys-air.wifi.local.cmu.edu>
2023-03-24 17:41:23 -0400

    test

--- runTest.py
+++ runTest.py
@@ -12,13 +12,14 @@ from testapp.models import Account, Transaction
 # The following can only be run once
 
 # Create a test user
-user1 = User.objects.create_user(username='testuser1', password='password1')
+# user1 = User.objects.create_user(username='testuser1', password='password1')
+user2 = User.objects.create_user(username='testuser2', password='password2')
 
 # Create test accounts
 
-Account.objects.create(account_number='1234567890', current_balance=1000.00,user = user1)
-Account.objects.create(account_number='0987654321', current_balance=500.00,user = user1)
+# Account.objects.create(account_number='1234567890', current_balance=1000.00,user = user1)
+# Account.objects.create(account_number='0987654321', current_balance=500.00,user = user1)
 
 # Create test transactions
-Transaction.objects.create(date='2021-01-01', transaction_type='CREDIT', note='test', amount=100.00, account=Account.objects.get(account_number='1234567890'))
-Transaction.objects.create(date='2021-01-01', transaction_type='CREDIT', note='test', amount=200.00, account=Account.objects.get(account_number='0987654321'))
+# Transaction.objects.create(date='2021-01-01', transaction_type='CREDIT', note='test', amount=100.00, account=Account.objects.get(account_number='1234567890'))
+# Transaction.objects.create(date='2021-01-01', transaction_type='CREDIT', note='test', amount=200.00, account=Account.objects.get(account_number='0987654321'))
